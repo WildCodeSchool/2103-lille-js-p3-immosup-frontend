9d224d4b0db64a4eccedfd3627eca2e3db125aa0
Kévin <bdtkevin@gmail.com>
2021-06-02 15:00:00 +0200

    add min-height in SApp.jsx

--- src/SApp.jsx
+++ src/SApp.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const SApp = styled.div`
-  background-color: #a9a9a9;
+  min-height: 100vh;
 `;
 
 export default SApp;
--- src/components/Footer/style.jsx
+++ src/components/Footer/style.jsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 
 const SFooter = styled.footer`
+  position: absolute;
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: #109fe6;
   color: #fff;
-  width: 100vw;
+  right: 0;
+  bottom: 0;
+  left: 0;
   height: 75px;
   padding: 0 20px;
 
