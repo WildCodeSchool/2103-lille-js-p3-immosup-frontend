4df69fa99b36ab924490f34f216bc459b986e0ba
frvanco <antoinevancompernolle@gmail.com>
2021-06-02 13:57:15 +0200

    add input for all info to sumbit

--- src/components/AddAnnonce/index.jsx
+++ src/components/AddAnnonce/index.jsx
@@ -1,101 +1,155 @@
+import { useState } from 'react';
+import axios from 'axios';
 import Addannoncestyle from './style';
 
-export default function AddAnnonce() {
+function AddAnnonce() {
+  const [inputDistrict, setDistrict] = useState('');
+  const [inputAdress, setAdress] = useState('');
+  const [inputCity, setCity] = useState('');
+  const [inputFurnished, setFurnished] = useState('');
+  const [inputRent, setRent] = useState('');
+  const [inputSurface, setSurface] = useState('');
+  const [inputAnimals, setAnimals] = useState('');
+  const [inputTitle, setTitle] = useState('');
+  const [inputCategory, setCategory] = useState('');
+  const [inputType, setType] = useState('');
+  const [inputEnergyClass, setEnergyClass] = useState('');
+  const [inputRooms, setRooms] = useState('');
+  const [inputDescribe, setDescribe] = useState('');
+
+  const handleChangeDistrict = (evt) => {
+    setDistrict(evt.target.value);
+  };
+
+  const handleChangeAdress = (evt) => {
+    setAdress(evt.target.value);
+  };
+
+  const handleChangeCity = (evt) => {
+    setCity(evt.target.value);
+  };
+
+  const handleChangeFurnished = (evt) => {
+    setFurnished(evt.target.value);
+  };
+
+  const handleChangeRent = (evt) => {
+    setRent(evt.target.value);
+  };
+
+  const handleChangeSurface = (evt) => {
+    setSurface(evt.target.value);
+  };
+
+  const handleChangeAnimals = (evt) => {
+    setAnimals(evt.target.value);
+  };
+
+  const handleChangeTitle = (evt) => {
+    setTitle(evt.target.value);
+  };
+
+  const handleChangeCategory = (evt) => {
+    setCategory(evt.target.value);
+  };
+
+  const handleChangeType = (evt) => {
+    setType(evt.target.value);
+  };
+
+  const handleChangeEnergyClass = (evt) => {
+    setEnergyClass(evt.target.value);
+  };
+
+  const handleChangeDescribe = (evt) => {
+    setDescribe(evt.target.value);
+  };
+
+  const handleChangeRooms = (evt) => {
+    setRooms(evt.target.value);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const dataToSend = {
+      district: inputDistrict,
+      adress: inputAdress,
+      city: inputCity,
+      furnished: inputFurnished,
+      rent: inputRent,
+      surface: inputSurface,
+      animals: inputAnimals,
+      title: inputTitle,
+      category: inputCategory,
+      type: inputType,
+      energyClass: inputEnergyClass,
+      rooms: inputRooms,
+      describe: inputDescribe,
+    };
+    axios.post('http://localhost:5050/addannonce', dataToSend);
+  };
+
   return (
     <Addannoncestyle>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Titre
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Catégorie:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Type de bien:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Meublé ou non:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Classe énergie:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Gaz a effet serre:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Nombre de pièces:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Nombre de metres carrés:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Prix:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
+      <form onSubmit={handleSubmit}>
+        <p>District:</p>
+        <input
+          type="text"
+          value={inputDistrict}
+          onChange={handleChangeDistrict}
+        />
+        <p>Adress:</p>
+        <input type="text" value={inputAdress} onChange={handleChangeAdress} />
+        <p>City:</p>
+        <input type="text" value={inputCity} onChange={handleChangeCity} />
+        <p>Furnished:</p>
+        <input
+          type="text"
+          value={inputFurnished}
+          onChange={handleChangeFurnished}
+        />
+        <p>Rent:</p>
+        <input type="text" value={inputRent} onChange={handleChangeRent} />
+        <p>Surface:</p>
+        <input
+          type="text"
+          value={inputSurface}
+          onChange={handleChangeSurface}
+        />
+        <p>Animals:</p>
+        <input
+          type="text"
+          value={inputAnimals}
+          onChange={handleChangeAnimals}
+        />
+        <p>Title:</p>
+        <input type="text" value={inputTitle} onChange={handleChangeTitle} />
+        <p>Category:</p>
+        <input
+          type="text"
+          value={inputCategory}
+          onChange={handleChangeCategory}
+        />
+        <p>Type:</p>
+        <input type="text" value={inputType} onChange={handleChangeType} />
+        <p>Energy class:</p>
+        <input
+          type="text"
+          value={inputEnergyClass}
+          onChange={handleChangeEnergyClass}
+        />
+        <p>Rooms:</p>
+        <input type="text" value={inputRooms} onChange={handleChangeRooms} />
+        <p>Describe:</p>
+        <input
+          type="text"
+          value={inputDescribe}
+          onChange={handleChangeDescribe}
+        />
 
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Description:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          Photo
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-        <input type="file" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          localisation:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      <form action="/data" method="post">
-        <label htmlFor="test">
-          coordonées:
-          <input type="text" name="name" id="test" />
-        </label>
-        <input type="submit" value="Envoyer" />
+        <input type="submit" value="Send!" />
       </form>
     </Addannoncestyle>
   );
 }
+export default AddAnnonce;
