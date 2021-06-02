1fa91dfd3e3bc98a27d0e6617d7e9cbf40544cde
Kévin <bdtkevin@gmail.com>
2021-06-02 10:06:55 +0200

    Add Roboto font & imgs

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -4,8 +4,19 @@ import SHomePage from './style';
 export default function HomePage() {
   return (
     <SHomePage>
-      <Link to="tenant/dashboard">Espace étudiants et jeunes dîplomés</Link>
-      <Link to="landlord/dashboard">Espace déposeurs d&apos;annonces</Link>
+      <header>
+        <div className="logo-container">
+          <img src="/img/logo/logoFull-1.png" className="logo" alt="logo" />
+        </div>
+        <div className="carousel">
+          <Link to="tenant/dashboard" className="link-dashboard tenant">
+            Espace étudiants et jeunes dîplomés
+          </Link>
+          <Link to="landlord/dashboard" className="link-dashboard landlord">
+            Espace déposeurs d&apos;annonces
+          </Link>
+        </div>
+      </header>
     </SHomePage>
   );
 }
--- src/components/HomePage/style.jsx
+++ src/components/HomePage/style.jsx
@@ -1,7 +1,27 @@
 import styled from 'styled-components';
 
 const SHomePage = styled.div`
-  background-color: lightcyan;
+  .logo-container {
+    text-align: center;
+    .logo {
+      width: 360px;
+      height: 180px;
+    }
+  }
+
+  .carousel {
+    background-image: url('/img/header-1.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 700px;
+
+    .link-dashboard {
+      background-color: #fff;
+      text-decoration: none;
+      color: #000;
+    }
+  }
 `;
 
 export default SHomePage;
