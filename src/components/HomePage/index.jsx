9295e14bacf1ede5d59b4bb76fb5b8e9643d19a4
Kévin <bdtkevin@gmail.com>
2021-06-02 10:39:18 +0200

    Add descrpitions and cdd

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -17,6 +17,32 @@ export default function HomePage() {
           </Link>
         </div>
       </header>
+      <div className="descriptions">
+        <section className="description first">
+          <h1 className="title">IMMO’SUP c’est quoi ?</h1>
+          <article className="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </article>
+        </section>
+        <section className="description second">
+          <h1 className="title">IMMO’SUP c’est quoi ?</h1>
+          <article className="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </article>
+        </section>
+      </div>
     </SHomePage>
   );
 }
--- src/components/HomePage/style.jsx
+++ src/components/HomePage/style.jsx
@@ -10,6 +10,9 @@ const SHomePage = styled.div`
   }
 
   .carousel {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
     background-image: url('/img/header-1.png');
     background-repeat: no-repeat;
     background-size: cover;
@@ -17,9 +20,55 @@ const SHomePage = styled.div`
     height: 700px;
 
     .link-dashboard {
+      display: inline-block;
       background-color: #fff;
       text-decoration: none;
       color: #000;
+      width: 400px;
+      height: 150px;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 40px;
+      line-height: 47px;
+      padding: 24px;
+      margin-bottom: 60px;
+
+      &.tenant {
+        text-align: left;
+      }
+
+      &.landlord {
+        text-align: right;
+      }
+    }
+  }
+
+  .descriptions {
+    .description {
+      width: 50vw;
+
+      &.first {
+        padding: 50px 5vw 50px 10vw;
+      }
+
+      &.second {
+        padding: 50px 10vw 50px 5vw;
+      }
+
+      .title {
+        font-style: normal;
+        font-weight: bold;
+        font-size: 40px;
+        line-height: 47px;
+        text-align: center;
+        margin: 20px;
+      }
+
+      .body {
+        text-align: justify;
+        font-size: 15px;
+        line-height: 18px;
+      }
     }
   }
 `;
