527c78b09650dcb0f31045204db201c407f9c08c
Kévin <bdtkevin@gmail.com>
2021-06-02 14:55:51 +0200

    add footer style & links

--- src/components/Footer/style.jsx
+++ src/components/Footer/style.jsx
@@ -1,7 +1,28 @@
 import styled from 'styled-components';
 
 const SFooter = styled.footer`
-  background-color: lightcoral;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #109fe6;
+  color: #fff;
+  width: 100vw;
+  height: 75px;
+  padding: 0 20px;
+
+  .link {
+    text-decoration: none;
+    color: inherit;
+    font-size: 15px;
+
+    &.immosup {
+      font-weight: 500;
+    }
+
+    &.contact {
+      margin-right: 20px;
+    }
+  }
 `;
 
 export default SFooter;
