c315c064ddd194ba51956ba73f7197326962d2af
Maenhout <victor@maenhout.fr>
2021-06-03 10:04:50 +0200

    Style additions for the 'personal' part of the dashboard

--- src/components/Tenant/TenantDashboard/index.jsx
+++ src/components/Tenant/TenantDashboard/index.jsx
@@ -4,13 +4,48 @@ import STenantDashboard from './style';
 export default function TenantDashboard() {
   return (
     <STenantDashboard>
-      <p>TenantDashboard</p>
-      <Link to="/tenant/profil">Profil</Link>
-      <Link to="/tenant/criteria">Criteria</Link>
-      <Link to="/tenant/match">Match</Link>
-      <Link to="/tenant/announcements-save">Announcements save</Link>
-      <Link to="/tenant/roommate-hunt">Roommate hunt</Link>
-      <Link to="/tenant/housing-search">Housing search</Link>
+      <h2>Tableau de bord</h2>
+
+      <div className="personal">
+        <div className="user">
+          <img
+            src="/Tenant/TenantDashbord/account_circle.png"
+            alt="avatar"
+            className="imgAvatar"
+          />
+          <h3 className="h3User">
+            Bienvenue
+            <br />
+            Nom de l&apos;utilisateur
+          </h3>
+        </div>
+
+        <Link to="/tenant/profil" className="buttonProfil">
+          <h3 className="h3Profil">Mon Profile</h3>
+        </Link>
+
+        <Link to="/tenant/criteria" className="buttonCriteria">
+          <h3 className="h3Criteria">Mes Critères</h3>
+        </Link>
+
+        <Link to="/" className="buttonDisconnected">
+          <img
+            src="/Tenant/TenantDashbord/logout_black_48dp.svg"
+            alt="Disconnected"
+            className="imgDisconnected"
+          />
+        </Link>
+      </div>
+
+      <div>
+        <Link to="/tenant/match">Mes Matchs</Link>
+        <Link to="/tenant/roommate-hunt">Roommate hunt</Link>
+      </div>
+
+      <div>
+        <Link to="/tenant/announcements-save">Announcements save</Link>
+        <Link to="/tenant/housing-search">Housing search</Link>
+      </div>
     </STenantDashboard>
   );
 }
--- src/components/Tenant/TenantDashboard/style.jsx
+++ src/components/Tenant/TenantDashboard/style.jsx
@@ -1,7 +1,82 @@
 import styled from 'styled-components';
 
+// const global ===================================
+
+const boxShadow = '4px 4px 10px rgba(0, 0, 0, 0.25)';
+
+// const button ===================================
+
+const colorButton = '#ffffff';
+const borderButton = '1px solid #e8e8e8';
+const paddingButton = '20px';
+
+// style of page ===================================
+
 const STenantDashboard = styled.div`
-  background-color: lightgreen;
+  background-color: #ffffff;
+  font-family: 'Roboto', serif;
+  a {
+    text-decoration: none;
+    color: #000000;
+    a:visited {
+      color: #000000;
+    }
+  }
+
+  h2 {
+    font-weight: bold;
+    font-size: 60px;
+  }
+
+  .personal {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .user {
+    display: flex;
+    align-items: center;
+    font-weight: bold;
+    .h3User {
+      font-size: 25px;
+    }
+    .imgAvatar {
+      width: 120px;
+    }
+  }
+
+  .buttonProfil {
+    padding: ${paddingButton};
+    background-color: ${colorButton};
+    border: ${borderButton};
+    box-shadow: ${boxShadow};
+    border-radius: 20px;
+    .h3Profil {
+      font-weight: bold;
+      font-size: 30px;
+    }
+  }
+
+  .buttonCriteria {
+    padding: ${paddingButton};
+    background-color: ${colorButton};
+    border: ${borderButton};
+    box-shadow: ${boxShadow};
+    border-radius: 20px;
+    .h3Criteria {
+      font-weight: bold;
+      font-size: 30px;
+    }
+  }
+
+  .buttonDisconnected {
+    padding: ${paddingButton};
+    background-color: ${colorButton};
+    border: ${borderButton};
+    box-shadow: ${boxShadow};
+    border-radius: 100%;
+  }
 `;
 
 export default STenantDashboard;
