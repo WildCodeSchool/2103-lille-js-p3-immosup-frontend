3e56c9a14f167b1d6f1b2bd0f5cd07a506ce8d53
Loredan <davidloredanvicentiu@gmail.com>
2021-06-07 10:17:53 +0200

    make filter Location

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import TenantDashboard from './components/TenantDashboard';
 import LandlordDashboard from './components/LandlordDashboard';
+import FiltreLogement from './components/FiltreLogement';
 import Footer from './components/Footer';
 import GlobalStyle from './GlobalStyle';
 import SApp from './SApp';
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/tenant/dashboard" component={TenantDashboard} />
         <Route path="/landlord/dashboard" component={LandlordDashboard} />
       </Switch>
+      <FiltreLogement />
       <Footer />
     </SApp>
   );
