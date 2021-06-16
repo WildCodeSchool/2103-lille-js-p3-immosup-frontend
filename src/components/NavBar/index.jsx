ef22f2f93ca8a9c19cecddc082666ab530027049
Maenhout <victor@maenhout.fr>
2021-06-16 12:10:33 +0200

    Add navbar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import SNavBar from './style';
+
+export default function Footer() {
+  return (
+    <SNavBar>
+      <Link to="/" className="link immosup">
+        IMMO’SUP
+      </Link>
+      <Link to="/tenant/dashboard" className="link TenantDashboard">
+        <img
+          src="/img/icons/account_circle_black_48dp.svg"
+          alt="Tableau de bord"
+        />
+      </Link>
+    </SNavBar>
+  );
+}
