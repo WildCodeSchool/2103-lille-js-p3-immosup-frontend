3515dee24ccf4f0d506725125da16cfeb96c392a
Zamomako <robin-delannoy_student2021@wilder.school>
2021-06-23 10:27:08 +0200

    Get all favorites added by users on page DashboardTenant and TenantAnnoucementSaved

--- src/components/Tenant/TenantAnnouncementsSave/index.jsx
+++ src/components/Tenant/TenantAnnouncementsSave/index.jsx
@@ -1,3 +1,46 @@
+import axios from 'axios';
+import { useState, useEffect, useContext } from 'react';
+import UserInfos from '../../../contexts/UserInfos';
+import NavBar from '../../NavBar';
+
 export default function TenantAnnouncementsSave() {
-  return <div>AnnouncementsSave</div>;
+  const [fav, setFav] = useState([]);
+  const { userInfos } = useContext(UserInfos);
+
+  useEffect(() => {
+    if (userInfos) {
+      axios
+        .get(`http://localhost:5050/api/favorites/${userInfos.id}`)
+        .then(({ data }) => {
+          console.log(data);
+          setFav(data);
+        });
+    }
+  }, [userInfos]);
+  return (
+    <div>
+      <NavBar />
+      <h1>AnnouncementsSave</h1>
+      {userInfos && (
+        <div>
+          {fav.map((toto) => {
+            return (
+              <div>
+                <p>
+                  {toto.title}
+                  <br />
+                  {toto.rent}€<br />
+                  {toto.district}
+                  <br />
+                  {toto.description}
+                  <br />
+                </p>
+                <img src={`${toto.url}`} alt="imgAccomodation" />
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/components/Tenant/TenantDashboard/index.jsx
+++ src/components/Tenant/TenantDashboard/index.jsx
@@ -1,110 +1,121 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import STenantDashboard from './style';
+import UserInfos from '../../../contexts/UserInfos';
+import NavBar from '../../NavBar';
 
 export default function TenantDashboard() {
   const [fav, setFav] = useState([]);
+  const { userInfos } = useContext(UserInfos);
 
   useEffect(() => {
-    axios.get(`http://localhost:5050/api/favorites`).then(({ data }) => {
-      console.log(data);
-      setFav(data);
-    });
-  }, []);
+    if (userInfos) {
+      axios
+        .get(`http://localhost:5050/api/favorites/${userInfos.id}`)
+        .then(({ data }) => {
+          console.log(data);
+          setFav(data);
+        });
+    }
+  }, [userInfos]);
   return (
-    <STenantDashboard>
-      <h2>Tableau de bord</h2>
+    <div>
+      <NavBar />
 
-      {/* personal ================================================= */}
+      <STenantDashboard>
+        <h2>Tableau de bord</h2>
 
-      <div className="personal">
-        <div className="user">
-          <div className="imgAvatar" />
-          <h3 className="h3User">
-            Bienvenue
-            <br />
-            &apos;UserName&apos;
-          </h3>
-        </div>
+        {/* personal ================================================= */}
 
-        <Link to="/tenant/profil" className="buttonProfil">
-          <h3 className="h3Profil">Mon profile</h3>
-        </Link>
-
-        <Link to="/tenant/criteria" className="buttonCriteria">
-          <h3 className="h3Criteria">Mes critères</h3>
-        </Link>
-
-        <Link to="/" className="buttonDisconnected">
-          <img
-            src="/Tenant/TenantDashbord/logout.svg"
-            alt="Disconnected"
-            title="Déconnexion"
-            className="imgDisconnected"
-          />
-        </Link>
-      </div>
-
-      {/* flatsharing ================================================= */}
-
-      <div className="flatsharing">
-        <div className="myMatch">
-          <Link to="/tenant/match">
-            <h3 className="h3Match">Mes matchs</h3>
-          </Link>
-          <div className="galleryMatch">
-            <div className="profilMatch" />
-            <div className="profilMatch" />
-            <div className="profilMatch" />
-            <div className="profilMatch" />
-            <div className="profilMatch" />
+        <div className="personal">
+          <div className="user">
+            <div className="imgAvatar" />
+            <h3 className="h3User">
+              Bienvenue
+              <br />
+              &apos;UserName&apos;
+            </h3>
           </div>
-        </div>
-        <Link to="/tenant/roommate-hunt">
-          <img
-            src="/Tenant/TenantDashbord/heart.svg"
-            alt="Roommate hunt"
-            title="Chasse aux colocs"
-            className="imgHunt"
-          />
-        </Link>
-      </div>
 
-      {/* announcements ================================================= */}
-
-      <div className="announcements">
-        <div className="housing">
-          <Link to="/tenant/announcements-save">
-            <h3 className="h3Announcements">Mes annonces sauvegardées</h3>
+          <Link to="/tenant/profil" className="buttonProfil">
+            <h3 className="h3Profil">Mon profile</h3>
           </Link>
-          <div className="galleryAnnouncements">
-            {fav.map((toto) => {
-              return (
-                <div className="profilAnnouncements">
-                  <img src={`${toto.url}`} alt="imgAccomodation" />
-                  <p>
-                    {toto.title}
-                    <br />
-                    {toto.rent}€<br />
-                    {toto.district}
-                    <br />
-                    <br />
-                  </p>
-                </div>
-              );
-            })}
-          </div>
+
+          <Link to="/tenant/criteria" className="buttonCriteria">
+            <h3 className="h3Criteria">Mes critères</h3>
+          </Link>
+
+          <Link to="/" className="buttonDisconnected">
+            <img
+              src="/Tenant/TenantDashbord/logout.svg"
+              alt="Disconnected"
+              title="Déconnexion"
+              className="imgDisconnected"
+            />
+          </Link>
         </div>
-        <Link to="/tenant/housing-search">
-          <img
-            src="/Tenant/TenantDashbord/search.svg"
-            alt="Housing search"
-            title="Recherche de logements"
-            className="search"
-          />
-        </Link>
-      </div>
-    </STenantDashboard>
+
+        {/* flatsharing ================================================= */}
+
+        <div className="flatsharing">
+          <div className="myMatch">
+            <Link to="/tenant/match">
+              <h3 className="h3Match">Mes matchs</h3>
+            </Link>
+            <div className="galleryMatch">
+              <div className="profilMatch" />
+              <div className="profilMatch" />
+              <div className="profilMatch" />
+              <div className="profilMatch" />
+              <div className="profilMatch" />
+            </div>
+          </div>
+          <Link to="/tenant/roommate-hunt">
+            <img
+              src="/Tenant/TenantDashbord/heart.svg"
+              alt="Roommate hunt"
+              title="Chasse aux colocs"
+              className="imgHunt"
+            />
+          </Link>
+        </div>
+
+        {/* announcements ================================================= */}
+
+        <div className="announcements">
+          <div className="housing">
+            <Link to="/tenant/announcements-save">
+              <h3 className="h3Announcements">Mes annonces sauvegardées</h3>
+            </Link>
+            <div className="galleryAnnouncements">
+              {fav.map((toto) => {
+                return (
+                  <div className="profilAnnouncements">
+                    <img src={`${toto.url}`} alt="imgAccomodation" />
+                    <p>
+                      {toto.title}
+                      <br />
+                      {toto.rent}€<br />
+                      {toto.district}
+                      <br />
+                      <br />
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <Link to="/tenant/housing-search">
+            <img
+              src="/Tenant/TenantDashbord/search.svg"
+              alt="Housing search"
+              title="Recherche de logements"
+              className="search"
+            />
+          </Link>
+        </div>
+      </STenantDashboard>
+    </div>
   );
 }
