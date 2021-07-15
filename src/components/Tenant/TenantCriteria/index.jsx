1b4ddc0cc5ec9ca10e32ab43ca4d7c71b94898eb
Loredan <davidloredanvicentiu@gmail.com>
2021-07-15 15:36:38 +0200

    modified css, classes, make page responsive

--- src/components/Tenant/TenantCriteria/index.jsx
+++ src/components/Tenant/TenantCriteria/index.jsx
@@ -39,124 +39,126 @@ const TenantCriteria = () => {
   return (
     <STenantCriteria>
       <h2 className="titleCriteria"> Mes Critéres</h2>
-      <div className="PhotoStyle">
-        <img
-          className="PhotoCriteria"
-          src="https://i.ibb.co/87SV21k/Capture-decran-2021-05-26-a-09-47-33.png"
-          alt="PhotoProfil"
-        />
+      <div className="pagestyle">
+        <div className="PhotoStyle">
+          <img
+            className="PhotoCriteria"
+            src="/image\photoprofil.jpg"
+            alt="PhotoProfil"
+          />
+        </div>
+        <form className="filterform" onSubmit={handleSubmit}>
+          <fieldset className="selectprop">
+            <label className="townstyle" htmlFor="crCity">
+              Ville de Recherche:
+              <select
+                className="SelectCity"
+                name="crCity"
+                type="text"
+                onChange={handleChange}
+              >
+                <option value="All" selected>
+                  Métropole lilloise
+                </option>
+                <option value="LilleCentre">Lille Centre</option>
+                <option value="LilleWazemmes">Lille Wazemmes</option>
+                <option value="Loos">Loos</option>
+              </select>
+            </label>
+            <label className="petsstyle" htmlFor="crPets">
+              Animaux:
+              <select
+                className="SelectPets"
+                name="crPets"
+                onChange={handleChange}
+              >
+                <option value={false}>Non Merci</option>
+                <option value selected>
+                  Ne me derange pas
+                </option>
+              </select>
+            </label>
+            <label className="genderStyle" htmlFor="crGender">
+              Genre:
+              <select
+                className="SelectGender"
+                name="crGender"
+                onChange={handleChange}
+              >
+                <option value="F">Femmes uniquement</option>
+                <option value="M">Homme uniquement</option>
+                <option value="A">Sans préférence</option>
+              </select>
+            </label>
+          </fieldset>
+          <fieldset className="numberprop">
+            <label className="cotenantstyle" htmlFor="crNbCotenantsMin">
+              Nombre de Colocataires:
+              <input
+                id="nbCotenantsMin"
+                name="crNbCotenantsMin"
+                onChange={handleChange}
+                type="number"
+                min="1"
+                max={form.crNbCotenantsMax}
+                value={form.crNbCotenantsMin}
+              />
+              <input
+                id="nbCotenantsMax"
+                name="crNbCotenantsMax"
+                onChange={handleChange}
+                type="number"
+                min={form.crNbCotenantsMin}
+                max="5"
+                value={form.crNbCotenantsMax}
+              />
+            </label>
+            <label className="cotenantStyle" htmlFor="crAgeCotenantsMin">
+              Age de Colocataires:
+              <input
+                id="ageCotenantsMin"
+                name="crAgeCotenantsMin"
+                onChange={handleChange}
+                type="number"
+                min="17"
+                max={form.crAgeCotenantsMax}
+                value={form.crAgeCotenantsMin}
+              />
+              <input
+                id="ageCotenantsMax"
+                name="crAgeCotenantsMax"
+                onChange={handleChange}
+                type="number"
+                min={form.crAgeCotenantsMin}
+                max="40"
+                value={form.crAgeCotenantsMax}
+              />
+            </label>
+            <label className="budgetStyle" htmlFor="crBudgetMin">
+              Prix Location:
+              <input
+                id="budgetMin"
+                name="crBudgetMin"
+                onChange={handleChange}
+                type="number"
+                min="200"
+                max={form.crBudgetMax}
+                value={form.crBudgetMin}
+              />
+              <input
+                id="budgetMax"
+                name="crBudgetMax"
+                onChange={handleChange}
+                type="number"
+                min={form.crBudgetMin}
+                max="800"
+                value={form.crBudgetMax}
+              />
+            </label>
+          </fieldset>
+          <input className="buttonprop" type="submit" value="Enregistrer" />
+        </form>
       </div>
-      <form className="filterform" onSubmit={handleSubmit}>
-        <fieldset className="selectprop">
-          <label className="townprop" htmlFor="crCity">
-            Ville de Recherche:
-            <select
-              className="SelectCity"
-              name="crCity"
-              type="text"
-              onChange={handleChange}
-            >
-              <option value="All" selected>
-                Métropole lilloise
-              </option>
-              <option value="LilleCentre">Lille Centre</option>
-              <option value="LilleWazemmes">Lille Wazemmes</option>
-              <option value="Loos">Loos</option>
-            </select>
-          </label>
-          <label htmlFor="crPets">
-            Animaux:
-            <select
-              className="SelectPets"
-              name="crPets"
-              onChange={handleChange}
-            >
-              <option value={false}>Non Merci</option>
-              <option value selected>
-                Ne me derange pas
-              </option>
-            </select>
-          </label>
-          <label htmlFor="crGender">
-            Genre:
-            <select
-              className="SelectGender"
-              name="crGender"
-              onChange={handleChange}
-            >
-              <option value="F">Femmes uniquement</option>
-              <option value="M">Homme uniquement</option>
-              <option value="A">Sans préférence</option>
-            </select>
-          </label>
-        </fieldset>
-        <fieldset className="numberprop">
-          <label htmlFor="crNbCotenantsMin">
-            Nombre de Colocataires:
-            <input
-              id="nbCotenantsMin"
-              name="crNbCotenantsMin"
-              onChange={handleChange}
-              type="number"
-              min="1"
-              max={form.cr_nbCotenantsMax}
-              value={form.cr_nbCotenantsMin}
-            />
-            <input
-              id="nbCotenantsMax"
-              name="crNbCotenantsMax"
-              onChange={handleChange}
-              type="number"
-              min={form.cr_nbCotenantsMin}
-              max="5"
-              value={form.cr_nbCotenantsMax}
-            />
-          </label>
-          <label htmlFor="crAgeCotenantsMin">
-            Age de Colocataires:
-            <input
-              id="ageCotenantsMin"
-              name="crAgeCotenantsMin"
-              onChange={handleChange}
-              type="number"
-              min="17"
-              max={form.ageCotenantsMax}
-              value={form.cr_ageCotenantsMin}
-            />
-            <input
-              id="ageCotenantsMax"
-              name="crAgeCotenantsMax"
-              onChange={handleChange}
-              type="number"
-              min={form.ageCotenantsMin}
-              max="40"
-              value={form.cr_ageCotenantsMax}
-            />
-          </label>
-          <label htmlFor="crBudgetMin">
-            Prix Location:
-            <input
-              id="budgetMin"
-              name="crBudgetMin"
-              onChange={handleChange}
-              type="number"
-              min="200"
-              max={form.budgetMax}
-              value={form.cr_budgetMin}
-            />
-            <input
-              id="budgetMax"
-              name="crBudgetMax"
-              onChange={handleChange}
-              type="number"
-              min={form.budgetMin}
-              max="800"
-              value={form.cr_budgetMax}
-            />
-          </label>
-        </fieldset>
-        <input type="submit" className="buttonprop" value="Enregistrer" />
-      </form>
     </STenantCriteria>
   );
 };
--- src/components/Tenant/TenantCriteria/style.jsx
+++ src/components/Tenant/TenantCriteria/style.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const STenantCriteria = styled.div`
+  font-family: 'Roboto', serif;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -10,6 +11,10 @@ const STenantCriteria = styled.div`
 
   border-radius: 2%;
 
+  .pagestyle {
+    box-shadow: 4px 4px 10px rgb(0 0 0 / 25%);
+    border-radius: 20px;
+  }
   .filterform {
     display: flex;
     flex-direction: column;
@@ -19,24 +24,65 @@ const STenantCriteria = styled.div`
   }
   .titleCriteria {
     margin-top: 25px;
+    margin-bottom: 20px;
     font-size: 1.6em;
     font-weight: bold;
-    text-align: center;
   }
   .PhotoCriteria {
+    width: 8vw;
     border-radius: 50%;
     margin-top: 25px;
-    margin-left: 25px;
+    margin-left: 40%;
   }
   .selectprop {
     display: flex;
     flex-direction: column;
     margin-bottom: 30px;
+    margin-left: 7%;
+  }
+  .SelectCity {
+    margin-bottom: 10px;
+    width: 150px;
+  }
+  .SelectPets {
+    margin-bottom: 10px;
+    width: 150px;
+  }
+  .SelectGender, .SelectPets, .SelectCity {
+    display flex;
+    margin-top:10px
   }
   .numberprop {
     display: flex;
-    flex-direction: column;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+  .townstyle,
+  .petsstyle,
+  .genderStyle,
+  .cotenantstyle,
+  .budgetStyle,
+  .cotenantStyle {
+    font-size: 1em;
+    font-weight: bold;
+  }
+  #nbCotenantsMin,
+  #nbCotenantsMax,
+  #ageCotenantsMin,
+  #ageCotenantsMax {
+    width: 40px;
+  }
+  #budgetMin,
+  #budgetMax {
+    width: 50px;
+  }
+  .buttonprop {
+    width: 120px;
+    align-self: center;
+    margin-top: 20px;
     margin-bottom: 20px;
+    background-color: #109fe6;
+    border-radius: 10px;
   }
 `;
 
