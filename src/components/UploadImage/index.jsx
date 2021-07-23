f4972bd3372587cf7998dd4d410895ab54cd6fd3
Hochart-dev <hochart.dev@gmail.com>
2021-07-23 16:07:15 +0200

    modificate UploadImage/index.jsx

--- src/components/UploadImage/index.jsx
+++ src/components/UploadImage/index.jsx
@@ -1,37 +1,45 @@
-import { useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import PropTypes from 'prop-types';
 
-function Upload() {
-  const [image, setImage] = useState({ preview: '', raw: '' });
-
-  const handleChange = (e) => {
-    if (e.target.files.length) {
-      setImage({
-        preview: URL.createObjectURL(e.target.files[0]),
-        raw: e.target.files[0],
-      });
-    }
-  };
+function Upload({ image, setImage }) {
+  // const handleChange = (e) => {
+  //   if (e.target.files.length) {
+  //     setImage({
+  //       preview: URL.createObjectURL(e.target.files[0]),
+  //       raw: e.target.files[0],
+  //     });
+  //   }
+  // };
+  /*
   const handleUpload = async (e) => {
     e.preventDefault();
-    console.log(image);
     const formData = new FormData();
     formData.append('image', image.raw);
 
     axios.post('http://localhost:5050/files', formData).then(() => {
-      /* console.log('yay'); */
     });
-  };
+  }; */
 
   return (
     <div className="App">
-      <form onSubmit={handleUpload}>
-        <input type="file" id="upload-button" onChange={handleChange} />
-        <input type="submit" value="Upload!" />
-      </form>
-      <img src={image} alt="" />
+      <input
+        type="file"
+        id="upload-button"
+        onChange={(e) => {
+          setImage({
+            preview: URL.createObjectURL(e.target.files[0]),
+            raw: e.target.files[0],
+          });
+        }}
+      />
+      <img src={image.preview} alt="" />
     </div>
   );
 }
+
+Upload.propTypes = {
+  image: PropTypes.shape.isRequired,
+  setImage: PropTypes.func.isRequired,
+};
 
 export default Upload;
